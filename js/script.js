ec115eb1f6701c1a32e4e04fa704b2a051e7a447
AzarielUr <fa.corneau@gmail.com>
2017-01-03 11:03:24 +0100

    Algorithme du jeu 1.0

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function machineASous(){
+    
+    var credit = document.getElementById("credit").value;
+    
+    if (credit > 0){
+        
+        credit -= 1;
+        
+        var a = Math.floor(Math.random()*4);
+        console.log("A = "+a);
+        
+        var b = Math.floor(Math.random()*4);
+        console.log("B = "+b);
+        
+        var c = Math.floor(Math.random()*4);
+        console.log("C = "+c);
+        
+        var d = Math.floor(Math.random()*4);
+        console.log("D = "+d);
+        
+        if (a == b && a ==c && a==d){
+            
+            credit += 5
+        }
+        
+        document.getElementById("credit").value = credit;
+    }
+    
+    else {
+        
+        alert("Erreur : Nombre de crédits épuisé");
+    }
+    
+    //document.getElementById("credit").value = prompt();
+}
