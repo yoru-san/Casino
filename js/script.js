c2bc2e983c57339da859ce9fb9a6454896dec8e8
AzarielUr <fa.corneau@gmail.com>
2017-01-10 09:08:53 +0100

    Ajout animation

--- js/script.js
+++ js/script.js
@@ -6,22 +6,20 @@ function machineASous(){
         
         credit -= 1;
         
+        document.getElementById("boutonStart").disabled = true;
+        animation();
+        
         var a = Math.floor(Math.random()*4);
-        console.log("A = "+a);
-        
         var b = Math.floor(Math.random()*4);
-        console.log("B = "+b);
-        
         var c = Math.floor(Math.random()*4);
-        console.log("C = "+c);
-        
         var d = Math.floor(Math.random()*4);
-        console.log("D = "+d);
         
         document.getElementById("img1").src = "image/img_" + a + ".png";
         document.getElementById("img2").src = "image/img_" + b + ".png";
         document.getElementById("img3").src = "image/img_" + c + ".png";
         document.getElementById("img4").src = "image/img_" + d + ".png";
+        
+
         
         if (a == b && a ==c && a==d){
             
@@ -38,6 +36,47 @@ function machineASous(){
     
     //document.getElementById("credit").value = prompt();
 }
+
+var isRunning = false;
+
+function animation(){
+
+    isRunning = true;
+    tick();
+    setTimeout(stop, 2000);
+    
+    
+}
+
+function tick(){
+    
+    if(isRunning){
+        
+        var a = Math.floor(Math.random()*4);
+        var b = Math.floor(Math.random()*4);
+        var c = Math.floor(Math.random()*4);
+        var d = Math.floor(Math.random()*4);
+        
+        document.getElementById("img1").src = "image/img_" + a + ".png";
+        document.getElementById("img2").src = "image/img_" + b + ".png";
+        document.getElementById("img3").src = "image/img_" + c + ".png";
+        document.getElementById("img4").src = "image/img_" + d + ".png";
+        
+        setTimeout(tick, 50);
+    }
+}
+
+/*function start(){
+    
+    isRunning = true;
+}*/
+
+function stop(){
+    
+    isRunning = false;
+    document.getElementById("boutonStart").disabled= false;
+}
+
 var rand = Math.floor(Math.random()*4);
 console.log(rand);
 
